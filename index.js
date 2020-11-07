085720b5542621c85ff1c96cf9972e4ed76914d0
Muhid Hossain <muhidhossain7@gmail.com>
2020-11-07 19:46:34 +0600

    back-end is ready

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const sk_test = process.env.SK_TEST
+
+const stripe = require("stripe")(sk_test);
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total, // subunits of the currency
+    currency: "usd",
+  });
+
+  // OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+// - Listen command
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Listening 4000'));
